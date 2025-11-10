905ac3aabad90b6e8b261a0cf6f5b17327fd4f03
Asyraf Arch <asyrafwebsite@gmail.com>
2025-11-10 19:23:30 +0800

    fix(parallax): use root-relative paths in asset() calls

--- src/components/parallaxBackground.jsx
+++ src/components/parallaxBackground.jsx
@@ -17,7 +17,7 @@ const ParallaxBackground = () => {
         <div
           className="absolute inset-0 w-full h-screen -z-50"
           style={{
-            backgroundImage: `url(${asset("assets/sky.jpg")})`,
+            backgroundImage: `url(${asset("/assets/sky.jpg")})`,
             backgroundPosition: "bottom",
             backgroundSize: "cover",
           }}
@@ -27,7 +27,7 @@ const ParallaxBackground = () => {
         <motion.div
           className="absolute inset-0 -z-40"
           style={{
-            backgroundImage: `url(${asset("assets/mountain-3.png")})`,
+            backgroundImage: `url(${asset("/assets/mountain-3.png")})`,
             backgroundPosition: "bottom",
             backgroundSize: "cover",
             y: mountain3Y,
@@ -38,7 +38,7 @@ const ParallaxBackground = () => {
         <motion.div
           className="absolute inset-0 -z-30"
           style={{
-            backgroundImage: `url(${asset("assets/planets.png")})`,
+            backgroundImage: `url(${asset("/assets/planets.png")})`,
             backgroundPosition: "bottom",
             backgroundSize: "cover",
             x: planetsX,
@@ -49,7 +49,7 @@ const ParallaxBackground = () => {
         <motion.div
           className="absolute inset-0 -z-20"
           style={{
-            backgroundImage: `url(${asset("assets/mountain-2.png")})`,
+            backgroundImage: `url(${asset("/assets/mountain-2.png")})`,
             backgroundPosition: "bottom",
             backgroundSize: "cover",
             y: mountain2Y,
@@ -60,7 +60,7 @@ const ParallaxBackground = () => {
         <motion.div
           className="absolute inset-0 -z-10"
           style={{
-            backgroundImage: `url(${asset("assets/mountain-1.png")})`,
+            backgroundImage: `url(${asset("/assets/mountain-1.png")})`,
             backgroundPosition: "bottom",
             backgroundSize: "cover",
             y: mountain1Y,
